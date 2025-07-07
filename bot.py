a7811981980ef82023df769d17a9a804bd109daf
Shinercraft <shinercraftyt@gmail.com>
2025-07-07 02:08:43 +0200

    heyo

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD_SECRET")
+TOKEN = os.getenv("DISCORD_SECRET") # heyo
 
 intents = discord.Intents.default()
 intents.message_content = True
